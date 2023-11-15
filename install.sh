610cd724dfcb9b86658ee2cd99321af627ed41df
dodo <dodo@pop-os.localdomain>
2023-11-15 22:34:15 +0100

    fix install

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ echo "Télechargement des dépandances"
 sudo add-apt-repository "deb http://ppa.launchpad.net/ubuntuhandbook1/conkymanager2/ubuntu focal main"
 sudo apt install conky -y && sudo apt install conky-manager2 -y
 sudo apt update
+sudo rm conky.conf
+sudo rm conky_no_x11.conf
 echo "Télechargement des dépandances terminé"
 echo "Télechargement des polices"
 echo "Télechargement Fira code"
 unzip Fira_Code.zip -d Fira_Code
-rm -R Fira_Code.zip
-mv Fira_Code /usr/share/fonts/truetype/
+sudo rm -R Fira_Code.zip
+sudo mv Fira_Code /usr/share/fonts/truetype/
 echo "Télechargement DS-Digital"
 wget  https://dl.dafont.com/dl/?f=ds_digital
-mv index.html?f=ds_digital ds_digital
-mv ds_digital /usr/share/fonts/truetype/ 
+sudo mv index.html?f=ds_digital ds_digital
+sudo mv ds_digital /usr/share/fonts/truetype/ 
 echo "Télechargement des polices terminé"
 echo "Installation terminé"
 echo "Lancement de conky-manager2"
